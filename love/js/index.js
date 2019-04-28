b79ae155584f187f47c99742b7263182830c8103
lqyasl <2642816105@qq.com>
2019-04-28 23:56:15 +0800

    Site updated: 2019-04-28 23:55:55

--- love/js/index.js
+++ love/js/index.js
@@ -1,6 +1,6 @@
 // 颜色随机函数
 function randomColor(){  
-	var rand = '#'+('00000'+(Math.random()*0x1000000<<0).tostring(16)).slice(-6); return rand; } 字体颜色随机 changefont(); function changefont() { var i="0;" str="如果，爱一个人，守一份爱情，可以在早春一起去踏青，可以在盛夏一起去赏荷，可以在浅秋一起去观月，可以在深冬一起去寻梅，不厌倦，却欢乐，不平凡，却平淡。那么，此生便无憾了。" ; arr="str.split('');" arr.foreach((item)> {
+	var rand = '#'+('00000'+(Math.random()*0x1000000<<0).tostring(16)).slice(-6); return rand; } 字体颜色随机 changefont(); function changefont() { var i="0;" str="愿你过得好，祝我也顺心。不谈亏欠，谢谢我们以前遇见。我遇见你，所以我喜欢你。我喜欢你，所以我想陪伴你。在成长的路上，我们从青涩走到成熟，感谢那是的相遇，因为我始终坚信命运在那时把你安排到我的世界，总有他的道理。在成长的路上，我们学会了少一些抱怨，多一些理解，反思自己更加进步。漫漫人生，你能够选取平庸，也能够选取轰轰烈烈，不忘初心，让每一天都能有新的世界。虽然我们日复一日地过着，但是生活迎接我们的每一秒都是新的。正如那句歌词：后来，我总算学会了如何去爱。可惜你早已远去，消失在人海。后来，终于在眼泪中明白，有些人一旦错过就不再。永远不会再重来，有一个男孩爱着那个女孩。渐渐地，你在我的心中成了一份美好的存在。如果，爱一个人，守一份爱情，可以在早春一起去踏青，可以在盛夏一起去赏荷，可以在浅秋一起去观月，可以在深冬一起去寻梅，不厌倦，却欢乐，不平凡，却平淡。那么，此生便无憾了。" ; arr="str.split('');" arr.foreach((item)> {
 		var span = document.createElement("span");
 		span.innerHTML = item;
 		span.classList.add('box');
@@ -21,4 +21,7 @@ function randomColor(){
 			clearInterval(timer);
 		}
 	},300);
-}</0).tostring(16)).slice(-6);>
+}
+
+
+</0).tostring(16)).slice(-6);>
